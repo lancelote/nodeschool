e83839c8eadf9f7bcb1e2507156ee02632539f0d
Pavel Karateev <karateev.pavel@ya.ru>
2017-07-30 20:41:33 +0300

    Updates countLines solution

--- learnyounode/countLines.js
+++ learnyounode/countLines.js
@@ -1,17 +1,11 @@
 /**
  * Count number of new lines in a given file
  */
-var arguments = process.argv;
-if (arguments.length < 3) {
-    console.log('Please specify the file!')
-} else {
-    var filePath = process.argv[2];
-    var fs = require('fs');
-    var buffer = fs.readFileSync(filePath);
-    // Or right into text without buffer
-    // var text = fs.readFileSync(process.argv[2], 'utf8')
-    var text = buffer.toString();
-    var lines = text.split('\n');
 
-    console.log(lines.length - 1);
-}
+var file = process.argv[2];
+var fs = require('fs');
+var buffer = fs.readFileSync(file);
+var text = buffer.toString();
+var lines = text.split('\n');
+
+console.log(lines.length - 1);
